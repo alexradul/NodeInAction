83c16bd55b841eb989d56456ed980b36fcb7c6d2
Aleksandar <aleksandar.radulovic@gmail.com>
2017-11-05 16:20:44 +0100

    Logic of the callback example has been broken down into several functions.

--- ch2/callback-example/blog-recent.js
+++ ch2/callback-example/blog-recent.js
@@ -1,26 +1,39 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((req, resp) => {
-  if (req.url === '/') {
-    fs.readFile('./titles.json', (err, data) => {
-      if (err) {
-        console.error(err);
-        resp.end('Server Error');
-      } else {
-        const titles = JSON.parse(data.toString());
-        fs.readFile('./template.html', (err, data) => {
-          if (err) {
-            console.error(err);
-            resp.end('Server Error');
-          } else {
-            const template = data.toString();
-            const html = template.replace('%', titles.join('</li><li>'));
-            resp.writeHead(200, {'Content-Type': 'text/html'})
-            resp.end(html);
-          }
-        })
-      }
-    })
+http.createServer((request, response) => {
+  if (request.url === '/') {
+      getTitles(response);
   }
 }).listen(8000, 'localhost');
+
+function getTitles(response) {
+  fs.readFile('./titles.json', (err, data) => {
+    if (err) {
+      handleError(response, err);
+    } else {
+      getTemplate(response, JSON.parse(data.toString()));
+    }
+  });
+}
+
+function getTemplate(response, titles) {
+  fs.readFile('./template.html', (err, data) => {
+    if (err) {
+      handleError(response, err)
+    } else {
+      htmlResponse(response, data.toString(), titles);
+    }
+  });
+}
+
+function htmlResponse(response, template, titles) {
+  const html = template.replace('%', titles.join('</li><li>'));
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.end(html);
+}
+
+function handleError(response, err) {
+  console.error(err);
+  response.end('Server Error');
+}
