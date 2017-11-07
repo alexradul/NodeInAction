0ff0ef395a909ff25a3da684d24a1446914edeb2
Aleksandar <aleksandar.radulovic@gmail.com>
2017-11-07 08:17:06 +0100

    Added GUI to the application. Node's ejs module, as well as suitable templates have been introduced in the application.

--- ch3/index.js
+++ ch3/index.js
@@ -18,7 +18,14 @@ app.get('/articles', (req, res, next) => {
   Article.all((err, articles) => {
     if (err)
       return next(err);
-    res.send(articles);
+    res.format({
+      html: () => {
+        res.render('articles.ejs', {articles: articles});
+      },
+      json: () => {
+        res.send(articles);
+      }
+    });
   });
 });
 
